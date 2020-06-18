be08de26f55ddd61e4c4c70643a377ea9d33043c
hwarim <polarcoke2@snu.ac.kr>
2020-06-18 16:12:26 +0900

    typo

--- execute.sh
+++ execute.sh
@@ -61,7 +61,7 @@ export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export OCRE_PEER_ADDRESS=localhost:7051
+export CORE_PEER_ADDRESS=localhost:7051
 
 peer lifecycle chaincode install fabcounter.tar.gz
 
@@ -69,7 +69,7 @@ echo "Installing chaincode at org2.peer0"
 export CORE_PEER_LOCALMSPID="Org2MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-export OCRE_PEER_ADDRESS=localhost:9051
+export CORE_PEER_ADDRESS=localhost:9051
 
 peer lifecycle chaincode install fabcounter.tar.gz
 
