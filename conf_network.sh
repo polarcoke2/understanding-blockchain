42a6faf6404fbe27ad66938af9e9a0cb0784dd58
hwarim <polarcoke2@snu.ac.kr>
2020-06-18 15:15:18 +0900

    changed network configuration

--- conf_network.sh
+++ conf_network.sh
@@ -2,7 +2,7 @@
 
 NETWORK=${PWD}/test-network
 cd $NETWORK
-docker stop logspout
+sudo docker stop logspout
 sudo chown -R $USER:$USER *
 ./network.sh down
 ./network.sh up createChannel -ca -s couchdb
