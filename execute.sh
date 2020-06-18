7df8fd3363f3f38be2d4533bb158a755600b008d
hwarim <polarcoke2@snu.ac.kr>
2020-06-18 16:59:25 +0900

    typo finally founded

--- execute.sh
+++ execute.sh
@@ -81,15 +81,17 @@ export PACKAGE_ID=`peer lifecycle chaincode queryinstalled | awk '/fabcounter_1:
 echo "Package ID is..."
 echo "$PACKAGE_ID"
 
-peer lifecycle chaincode approvefromyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -C --channelID mychannel --name fabcounter --version 1.0 --package-id $PACKAGE_ID --sequence 1 --tls \
---cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+echo "approving from org2"
+peer lifecycle chaincode approveformyorg -o localhost:7050 --channelID mychannel --name fabcounter --version 1.0 --package-id $PACKAGE_ID --sequence 1 --tls \
+--cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --ordererTLSHostnameOverride orderer.example.com
 
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export OCRE_PEER_ADDRESS=localhost:7051
+export CORE_PEER_ADDRESS=localhost:7051
 
-peer lifecycle chaincode approvefromyorg -o localhost:7050 \
+echo "approving from org1"
+peer lifecycle chaincode approveformyorg -o localhost:7050 \
 --ordererTLSHostnameOverride orderer.example.com \
 --channelID mychannel \
 --name fabcounter \
